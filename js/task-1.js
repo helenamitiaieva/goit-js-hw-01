ef3760175d9b7a3fcb7f93be99fc04a08d8b1df6
hellena0606 <h0636641547@gmail.com>
2024-03-17 15:11:42 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
 
 function makeTransaction(quantity, pricePerDroid) {
-    console.log (`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
-    return quantity, pricePerDroid;
+    return console.log(`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`);
 };
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+ 
+makeTransaction(5, 3000); // "You ordered 5 droids worth 15000 credits!"
+makeTransaction(3, 1000); // "You ordered 3 droids worth 3000 credits!"
+makeTransaction(10, 500); // "You ordered 10 droids worth 5000 credits!"
